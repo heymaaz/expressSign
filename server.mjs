ec3d7344b6be5c40e304fd88c63317bbddf5e4fe
Maaz Chowdhry <maazchowdhry2001@gmail.com>
2024-04-19 18:37:57 +0100

    Add logging to server.mjs and changed default port to 8080

--- server.mjs
+++ server.mjs
@@ -3,11 +3,21 @@ import cors from 'cors';
 import { downloadSubtitles } from './ytdlp.mjs';
 import { parseSubtitles } from './subtitleParser.mjs';
 import { subToGloss } from './subToGloss.mjs';
+import e from 'express';
 
 // Create a new express application instance
 
 const app = express();
 app.use(cors());
+
+// log the requests
+app.use((req, res, next) => {
+    // get current time
+    const now = new Date();
+    // log the request method and url
+    console.log(`${now.toISOString()} ${req.method} ${req.url}`);
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -23,14 +33,18 @@ app.get('/subtitles/:videoID', async (req, res) => {
     const videoUrl = `https://www.youtube.com/watch?v=${req.params.videoID}`;
     const sub_file_path = await downloadSubtitles(videoUrl);
     if (!sub_file_path) {
+        console.log(errorDownload);
         res.json(errorDownload);
         return;
     }
     const subtitles = await parseSubtitles(sub_file_path);
     if (!subtitles) {
+        console.log(errorParse);
         res.json(errorParse);
         return;
     }
+    let now = new Date();
+    console.log(`${now.toISOString()} ${req.method} ${req.url} ${subtitles.length} subtitles`);
     res.json(subtitles);
 });
 
@@ -39,34 +53,40 @@ app.get('/gloss/:videoID', async (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     const errorDownload = { error: 'Failed to download subtitles' };
     const errorParse = { error: 'Failed to parse subtitles' };
+    const errorGloss = { error: 'Failed to get glosses' };
     // Download the subtitles for the video
     const videoUrl = `https://www.youtube.com/watch?v=${req.params.videoID}`;
     const sub_file_path = await downloadSubtitles(videoUrl);
     if (!sub_file_path) {
+        console.log(errorDownload);
         res.json(errorDownload);
         return;
     }
     const subtitles = await parseSubtitles(sub_file_path);
     if (!subtitles) {
+        console.log(errorParse);
         res.json(errorParse);
         return;
     }
     // get the glosses json using subToGloss function
     const glosses = await subToGloss(subtitles, req.params.videoID);
     if (!glosses) {
-        res.json({ error: 'Failed to get glosses' });
+        console.log(errorGloss);
+        res.json(errorGloss);
         return;
     }
+    let now = new Date();
+    console.log(`${now.toISOString()} ${req.method} ${req.url} ${glosses.length} glosses`);
     res.json(glosses);
 });
 
 // Start the Express server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     // log the url
-    const url = `http://localhost:${port}`;
+    let url = `http://localhost:${port}`;
     if (process.env.URL) {
-        url = `https://${process.env.URL}`;
+        url = `http://${process.env.URL}:${port}`;
     }
     console.log(`Server running at ${url}`);
 });
